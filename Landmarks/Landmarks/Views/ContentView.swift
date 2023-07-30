bd571836ca282db0d191a660595ed48ea0815ba8
realhsb <realhsb@gmail.com>
2023-07-30 21:07:22 +0900

    ✨ Content View .environmentObject() 속성 추가

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Subeen on 2023/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
